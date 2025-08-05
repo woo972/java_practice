dc18f665b7bcd6d823e2cce3a5abd5bea246e934
sookeun.woo <dntnrmsss@gmail.com>
2025-08-05 18:05:33 +0900

    add spring dependencies

--- reactive_programming/build.gradle.kts
+++ reactive_programming/build.gradle.kts
@@ -16,4 +16,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     implementation("org.projectlombok:lombok:1.18.22")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
