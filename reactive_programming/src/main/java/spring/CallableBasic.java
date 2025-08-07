b918aced1e64a527c8959c050ba91b24471ef75c
sookeun.woo <dntnrmsss@gmail.com>
2025-08-07 23:34:31 +0900

    callable 설명 추가

--- reactive_programming/src/main/java/spring/CallableBasic.java
+++ reactive_programming/src/main/java/spring/CallableBasic.java
@@ -22,6 +22,31 @@ public class CallableBasic {
     }
 
     // callable을 통해 요청 스레드를 block하지 않고 먼저 리턴한 후에 worker thread를 실행
+    /*
+
+    Client 요청
+   ↓
+   [Acceptor] → 연결 수락
+     ↓
+    [Poller] → 요청이 읽기 가능 상태인지 확인
+     ↓
+    [Executor] → 실제 요청 처리 (ex: Controller 실행)
+   ↓
+    Tomcat Executor 스레드 → DispatcherServlet
+   ↓
+    Controller에서 Callable 반환
+   ↓
+    Tomcat 스레드 반환됨 (풀로 복귀)
+   ↓
+    Spring 내부 TaskExecutor가 별도 스레드로 Callable 실행
+   ↓
+    작업 완료 시 DispatcherServlet으로 복귀 → 응답 전송
+
+
+
+    * */
+
+
     @GetMapping("/callable")
     public Callable<String> callable() {
         // http-nio-8080-exec-4
